747b74d5c7791470b03cce50b9dcd64fed3d1300
leeshanex <leeshanex@outlook.com>
2020-05-18 12:41:47 -0700

    Superhero class - KEY Model created, Added a property type of DbSet<>, Add-Migration and updated Database, Created SuperheroController

--- Superhero_Proj/Data/ApplicationDbContext.cs
+++ Superhero_Proj/Data/ApplicationDbContext.cs
@@ -3,11 +3,13 @@ using System.Collections.Generic;
 using System.Text;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
+using Superhero_Proj.Models;
 
 namespace Superhero_Proj.Data
 {
     public class ApplicationDbContext : IdentityDbContext
     {
+        public DbSet<Superhero> Superheroes { get; set; }
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
---
+++ Superhero_Proj/Models/Superhero.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Superhero_Proj.Models
+{
+    public class Superhero 
+    {
+        [Key]
+        public int ID { get; set; }
+    }
+}
