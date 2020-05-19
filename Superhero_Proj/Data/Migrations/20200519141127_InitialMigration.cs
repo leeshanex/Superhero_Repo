31c4f754791d086f7482eb28635ef0ff552d30fc
leeshanex <leeshanex@outlook.com>
2020-05-19 07:19:33 -0700

    Recreated a initialmigration with updated properties to superhero class - however missing int ID

---
+++ Superhero_Proj/Data/Migrations/20200519141127_InitialMigration.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Superhero_Proj.Data.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "alterEgoName",
+                table: "Superheroes",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "catchphrase",
+                table: "Superheroes",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "primaryAbility",
+                table: "Superheroes",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "secondaryAbility",
+                table: "Superheroes",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "superheroName",
+                table: "Superheroes",
+                nullable: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "alterEgoName",
+                table: "Superheroes");
+
+            migrationBuilder.DropColumn(
+                name: "catchphrase",
+                table: "Superheroes");
+
+            migrationBuilder.DropColumn(
+                name: "primaryAbility",
+                table: "Superheroes");
+
+            migrationBuilder.DropColumn(
+                name: "secondaryAbility",
+                table: "Superheroes");
+
+            migrationBuilder.DropColumn(
+                name: "superheroName",
+                table: "Superheroes");
+        }
+    }
+}
